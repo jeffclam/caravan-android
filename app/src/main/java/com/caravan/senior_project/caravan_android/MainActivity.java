6e5bf38a7639574e59169c5b9cd9c30a32ce183a
Rohin C <Rohin C>
2017-02-02 14:03:58 -0800

    Added search bar with autocomplete with marker placed on selection

--- app/src/main/java/com/caravan/senior_project/caravan_android/MainActivity.java
+++ app/src/main/java/com/caravan/senior_project/caravan_android/MainActivity.java
@@ -13,18 +13,24 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.mapbox.geocoder.MapboxGeocoder;
-import com.mapbox.geocoder.android.AndroidGeocoder;
 import com.mapbox.mapboxsdk.MapboxAccountManager;
 import com.mapbox.mapboxsdk.annotations.Marker;
 import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.services.android.geocoder.ui.GeocoderAutoCompleteView;
+import com.mapbox.services.commons.models.Position;
+import com.mapbox.services.geocoding.v5.GeocodingCriteria;
+import com.mapbox.services.geocoding.v5.models.CarmenFeature;
+
 
 public class MainActivity extends AppCompatActivity {
     private MapView mapView;
+    private MapboxMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,12 +54,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onMapReady(MapboxMap mapboxMap) {
 
-                // Customize map with markers, polylines, etc.
-                /*MapboxGeocoder client = new MapboxGeocoder.Builder()
-                        .setAccessToken("pk.eyJ1IjoibmFuY3l0cnVvbmciLCJhIjoiY2l5a254N2kxMDAxdDJxczc3ejRwbHlweSJ9.-zGT-uLt61yIz6hpROnZ5Q")
-                        .setLocation("The White House")
-                        .build();*/
-                final Marker marker = mapboxMap.addMarker(new MarkerOptions()
+                map = mapboxMap;
+                /*final Marker marker = mapboxMap.addMarker(new MarkerOptions()
                         .position(new LatLng(38.897, -77.036))
                         .title("The White House")
                         .snippet("Home of the last surviving Oompa Loompa"));
@@ -69,10 +71,39 @@ public class MainActivity extends AppCompatActivity {
                         markerAnimator.setDuration(2000);
                         markerAnimator.start();
                     }
-                });
+                });*/
             }
         });
 
+        GeocoderAutoCompleteView autocomplete = (GeocoderAutoCompleteView) findViewById(R.id.query);
+        autocomplete.setAccessToken(MapboxAccountManager.getInstance().getAccessToken());
+        autocomplete.setType(GeocodingCriteria.TYPE_POI);
+        autocomplete.setOnFeatureListener(new GeocoderAutoCompleteView.OnFeatureListener() {
+            @Override
+            public void OnFeatureClick(CarmenFeature feature) {
+                Position position = feature.asPosition();
+                updateMap(position.getLatitude(), position.getLongitude());
+                /*double lat = position.getLatitude();
+                double lng = position.getLongitude();
+                map.addMarker(new MarkerOptions()
+                .position(new LatLng(lat, lng)));*/
+            }
+        });
+
+    }
+
+    private void updateMap(double latitude, double longitude) {
+        // Build marker
+        map.addMarker(new MarkerOptions()
+                .position(new LatLng(latitude, longitude))
+                .title("Geocoder result"));
+
+        // Animate camera to geocoder result location
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(new LatLng(latitude, longitude))
+                .zoom(15)
+                .build();
+        map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition), 5000, null);
     }
 
     @Override
